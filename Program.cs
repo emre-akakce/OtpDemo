d77b9e9b75ce5c25825b0c83e7d84fc42b9e3df9
emre-akakce <emre@akakce.com>
2025-06-19 09:35:47 +0300

    use library

--- Program.cs
+++ Program.cs
@@ -1,29 +1,18 @@
-﻿using System.Security.Cryptography;
+﻿using System;
+using OtpNet;
 
 class Program
 {
     static void Main()
     {
-        string otp = GenerateNumericOtp(6);
-        Console.WriteLine($"Your OTP is: {otp}");
-        // You can now return or send this OTP to the user
-    }
 
-    public static string GenerateNumericOtp(int length)
-    {
-        if (length <= 0)
-            throw new ArgumentException("OTP length must be positive");
+        byte[] secretKey = Base32Encoding.ToBytes("JBSWY3DPEHPK3PXP");
 
-        var otp = new char[length];
-        using var rng = RandomNumberGenerator.Create();
-        byte[] buffer = new byte[1];
 
-        for (int i = 0; i < length; i++)
-        {
-            rng.GetBytes(buffer);
-            otp[i] = (char)('0' + (buffer[0] % 10)); // Generates digits 0–9
-        }
+        var totp = new Totp(secretKey, mode: OtpHashMode.Sha256);
 
-        return new string(otp);
+        string otp = totp.ComputeTotp(); // e.g., "123456"
+
+        Console.WriteLine($"Current TOTP: {otp}");
     }
 }
