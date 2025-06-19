eb1c834483bd8594fcf6ab1d8e8ffa175a9de3cb
emre-akakce <emre@akakce.com>
2025-06-19 09:17:09 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Security.Cryptography;
+
+class Program
+{
+    static void Main()
+    {
+        string otp = GenerateNumericOtp(6);
+        Console.WriteLine($"Your OTP is: {otp}");
+        // You can now return or send this OTP to the user
+    }
+
+    public static string GenerateNumericOtp(int length)
+    {
+        if (length <= 0)
+            throw new ArgumentException("OTP length must be positive");
+
+        var otp = new char[length];
+        using var rng = RandomNumberGenerator.Create();
+        byte[] buffer = new byte[1];
+
+        for (int i = 0; i < length; i++)
+        {
+            rng.GetBytes(buffer);
+            otp[i] = (char)('0' + (buffer[0] % 10)); // Generates digits 0–9
+        }
+
+        return new string(otp);
+    }
+}
